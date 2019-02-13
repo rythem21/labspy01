25e20bef01df5728bb6585356d146c3e44374705
Iqbal Prayogi <iqbalprayogi92@gmail.com>
2019-02-13 21:38:00 +0700

    Upload Via Gitbash

---
+++ terbesar.py
@@ -0,0 +1,16 @@
+print("Mencari Nilai terbesar")
+bil1 = input("\nMasukkan bilangan pertama = ")
+bil2 = input("\nMasukkan bilangan kedua = ")
+bil3 = input("\nMasukkan bilangan ketiga = ")
+
+if(bil1>bil2 and bil1>bil3):
+	terbesar=bil1
+	print("\nBilangan terbesar = ",terbesar)
+
+if(bil2>bil1 and bil2>bil3):
+	terbesar=bil2
+	print("\nBilangan terbesar = ",terbesar)
+
+if(bil3>bil1 and bil3>bil1):
+	terbesar=bil3
+	print("\nBilangan terbesar = ",terbesar)
